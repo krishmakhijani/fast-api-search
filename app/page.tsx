429f084b35155ff4999ab42ba29ad7e46771414f
Krish Makhijani <112251957+krish-mm@users.noreply.github.com>
2024-09-09 01:58:16 +0530

    Base useEffect and useState implemented

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,32 @@
+"use client"
+
+import { useEffect, useState } from "react";
+
+
+
 export default function Home() {
+
+  const[input,setInput] = useState<string>('')
+  const[searchResult,setsearchResult] = useState<{
+    results: string[]
+    duration: number
+  }>()
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if(!input) return setsearchResult(undefined)
+      
+      const res = await fetch(`/api/search?q=${input}`)
+    }
+    fetchData()
+  })
+
+
   return (
     <div >
-      Hii
+      <input value={input} onChange={(e) => {
+        setInput(e.target.value)
+      }} type="text"/>
     </div>
   );
 }
